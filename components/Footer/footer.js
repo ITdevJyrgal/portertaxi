05150fc6c3f069a71a203f7800513aba0093a927
ITdevJyrgal <jyldyzbekuulujyrgalbek@gmail.com>
2022-05-04 10:05:27 +0600

    idnex

--- components/Footer/footer.js
+++ components/Footer/footer.js
@@ -24,7 +24,7 @@ export default function Footer() {
                     <div className="footer-general2__motion">
                         <h1>Все права защищены - 2022</h1>
                     </div>
-                    <a href="https://www.motion-web.io/">
+                    <a href="https://www.motion-web.io/" target="_blank">
                         <div className="footer-general2__motion2">
 
                    <span>
--- components/advan/advan.js
+++ components/advan/advan.js
@@ -1,25 +1,41 @@
+import one from "./../../assets/img/1.png";
+import two from "./../../assets/img/2.png";
+import tre from "./../../assets/img/3.png";
+import fo from "./../../assets/img/4.png";
+
 import Image from "next/image";
-import advandata from "./advandata";
+
 
 export default function Advan() {
     return (
         <section className="advan">
             <div className="container">
                 <div className="advan-general">
-                    {
-                        advandata.map(el => {
-                            return (
-                                <div key={el.id}>
-                                    <div className="advan-general__block">
-                                        <Image src={el.images} alt="img"/>
-                                        <p>{el.title}</p>
-                                    </div>
-                                    {/*<h1>{el.item}</h1>*/}
-                                </div>
-                            )
-                        })
-                    }
+                    <div className="advan-general__block">
+                        <Image src={one} alt="img"/>
+                        <p>Надёжная и бережная доставка</p>
+                    </div>
+                    <div className="advan-general__title">
+                        <h1>Преи <span>мущес</span>тва</h1>
+                    </div>
+                    <div className="advan-general__block">
+                        <Image src={two} alt="img"/>
+                        <p>Саблюдаем дорожные правила и этикет</p>
+                    </div>
+
                 </div>
+                <div className="advan-general2">
+                    <div className="advan-general__block">
+                        <Image src={tre} alt="img"/>
+                        <p>5 лет опыта работы</p>
+                    </div>
+                    <div className="advan-general__block">
+                        <Image src={fo} alt="img"/>
+                        <p>На месте когда и где нужно</p>
+                    </div>
+
+                </div>
+                <div className="advan-general2__width"/>
             </div>
         </section>
     )
