072d523853bdfe2207fb45d4699de9cf4d78466f
ITdevJyrgal <>
2022-05-07 22:52:21 +0600

    dine

--- components/Header/burgermenu.js
+++ components/Header/burgermenu.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, {useState} from "react";
 import * as FaIcons from "react-icons/fa";
 import Link from "next/link"
 
@@ -13,7 +13,7 @@ function Navbar() {
         <>
             <div className="navbar">
                 <div to="#" className="menu-bars" onClick={showSideMenu}>
-                    <FaIcons.FaBars className="menu-icon" />
+                    <FaIcons.FaBars className="menu-icon"/>
                 </div>
                 <nav className={sidebar ? "nav-menu active" : "nav-menu"}>
                     <ul className="nav-menu-items">
@@ -55,17 +55,17 @@ function Navbar() {
 
                     </ul>
                     <div>
-                    <div className="header-general__square">
-                        <i className="fab fa-telegram"/>
-                        <i className="fab fa-whatsapp"/>
-                        <a href="tel:0504416833">0504-41-68-33</a>
+                        <div className="header-general__square">
+                            <i className="fab fa-telegram"/>
+                            <i className="fab fa-whatsapp"/>
+                            <a href="tel:0504416833">0504-41-68-33</a>
+                        </div>
+                        <div className="header-general__square2">
+                            <i className="fab fa-telegram"/>
+                            <i className="fab fa-whatsapp"/>
+                            <a href="tel:0509-13-99-66">0509-13-99-66</a>
+                        </div>
                     </div>
-                    <div className="header-general__square2">
-                        <i className="fab fa-telegram"/>
-                        <i className="fab fa-whatsapp"/>
-                        <a href="tel:0509-13-99-66">0509-13-99-66</a>
-                    </div>
-                </div>
                 </nav>
             </div>
         </>
