efe9e51947b5c09da744011a58d081a6010cd6dc
LimitLless <limitlessconnection.uae@gmail.com>
2022-05-10 19:20:59 +0600

    idenx

--- components/Form/form.js
+++ components/Form/form.js
@@ -1,4 +1,4 @@
-import {useState} from "react";
+import {useState, useTransition} from "react";
 import Image from "next/image";
 import check from "./../../assets/img/check.png"
 import React from "react";
@@ -9,18 +9,38 @@ import axios from "axios"
 
 export default function Form() {
     const [active, setActive] = useState(false)
+    const [checkError, setCheckError] = useState('')
 
 
-    const {
-        register,
-        handleSubmit,
-        watch,
-        formState: {errors}
-    } = useForm();
+    const {register, handleSubmit, watch, formState: {errors}} = useForm();
+    const onSubmit = async (data) => {
+        alert(JSON.stringify(data))
+        //     fetch("https://porter-project.herokuapp.com/feedback/", {
+        //     method: 'POST', // или 'PUT'
+        //     body: data, // данные могут быть 'строкой' или {объектом}!
+        // })
+        //     .then(() => alert("Ваш запрос успешно отправлено"))
 
-    const onSubmit = (data) => {
-        alert(JSON.stringify(data));
+        try {
+            await axios.post('https://porter-project.herokuapp.com/feedback/', {
+
+                name: data.name,
+                phone: data.phone,
+                poter: data.poter,
+                movers: data.movers,
+                dispersal: data.dispersal,
+                trash: data.trash,
+                description: data.description
+
+            })
+                .then(() => alert("Ваш запрос успешно отправлено"))
+        } catch (e) {
+            console.log(e)
+        }
+
+
     };
+
 
     console.log(watch("example"));
 
@@ -42,18 +62,19 @@ export default function Form() {
                                 <label className="form-general__input"
                                        onSubmit={handleSubmit(onSubmit)}>
                                     <input type="text" name="name" placeholder="Ф.И.О"
-                                           className={errors.name ? "form-general__input__input__error" : "form-general__input__input"}
-                                           {...register("firstName", {
+                                           style={errors.name && {borderBottom: " 2px solid red"}}
+                                           className={"form-general__input__input"}
+                                           {...register("name", {
                                                required: true,
                                                maxLength: 20,
-                                               pattern: /^[A-Za-z]+$/i
                                            })}
                                     />
 
 
                                     <input type="number" name="tel" placeholder="Номер вашего телефона"
+                                           style={errors.phone && {borderBottom: " 2px solid red"}}
                                            className="form-general__input__input"
-                                           {...register("Number", {pattern: /^[A-Za-z]+$/i})}
+                                           {...register("phone", {required: true})}
                                     />
 
                                 </label>
@@ -63,48 +84,52 @@ export default function Form() {
                                     <h1>Выберите <span>подходящие</span> услуги</h1>
                                 </div>
                                 <div className="form-general__radio">
-                                 <div>
-                                     <label>
-                                         <input type="checkbox" name="radio"
+                                    <div>
+                                        <label>
+                                            <input type="checkbox" name="radio"
+                                                   {...register('poter')}
 
-                                                className={errors.name ? "form-general__radio__input__errors" : "form-general__radio__input"}
+                                                   className={errors.poter ? "form-general__radio__input__errors" : "form-general__radio__input"}
 
-                                         />
-                                         <p>Портер</p>
-                                     </label>
-                                     <label>
-                                         <input type="checkbox" name="radio"
+                                            />
+                                            <p>Портер</p>
+                                        </label>
+                                        <label>
+                                            <input type="checkbox" name="radio"
+                                                   {...register('movers')}
+                                                   className={errors.name ? "form-general__radio__input__errors" : "form-general__radio__input"}/>
+                                            <p>Грузчики</p>
+                                        </label>
+                                    </div>
+                                    <div>
+                                        <label>
+                                            <input type="checkbox" name="radio"
+                                                   {...register('dispersal')}
+                                                   className={errors.name ? "form-general__radio__input__errors" : "form-general__radio__input"}/>
+                                            <p>Разборка/сборка мебели</p>
+                                        </label>
+                                        <label>
+                                            <input type="checkbox" name="radio"
+                                                   {...register('trash')}
+                                                   className={errors.name ? "form-general__radio__input__errors" : "form-general__radio__input"}/>
+                                            <p>Вывоз мусора</p>
+                                        </label>
+                                    </div>
 
-                                                className={errors.name ? "form-general__radio__input__errors" : "form-general__radio__input"}/>
-                                         <p>Грузчики</p>
-                                     </label>
-                                 </div>
-                                <div>
-                                    <label>
-                                        <input type="checkbox" name="radio"
-
-                                               className={errors.name ? "form-general__radio__input__errors" : "form-general__radio__input"}/>
-                                        <p>Разборка/сборка мебели</p>
-                                    </label>
-                                    <label>
-                                        <input type="checkbox" name="radio"
-
-                                               className={errors.name ? "form-general__radio__input__errors" : "form-general__radio__input"}/>
-                                        <p>Вывоз мусора</p>
-                                    </label>
                                 </div>
-
+                                <div style={{textAlign: "center", padding: "20px 0"}}>
+                                    <span>{checkError}</span>
                                 </div>
                                 <label className="form-general__area">
-                                    <input type="text" placeholder="Комментарии к заказу (необязательно)" name='text'/>
+                                    <input type="text" placeholder="Комментарии к заказу (необязательно)"
+                                           name='text' {...register("description")}/>
                                 </label>
-                           <div className="form-general__submit">
-                               <input type="submit"
-                                      onClick={() => setActive(true)}
-                                      className="form-general__btn"
-                                      placeholder="Заказать"
-                               />
-                           </div>
+                                <div className="form-general__submit">
+                                    <input type="submit"
+                                           className="form-general__btn"
+                                           placeholder="Заказать"
+                                    />
+                                </div>
                             </label>
                         </form>
 
